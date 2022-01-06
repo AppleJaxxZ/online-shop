613bff5bb8c9baae27928482d78cca78e37659ac
Alex Zajacek <smackanalex@gmail.com>
2022-01-06 09:27:08 -0500

    Cart icon added, cart dropdownm, also added cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,10 +4,13 @@ import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
+import toggleCartHidden from '../../redux/cart/cart.actions';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
     console.log(currentUser, 'from the heaer')
     return (
         <div className='header'>
@@ -23,16 +26,19 @@ const Header = ({ currentUser }) => {
                 </Link>
                 {currentUser ? (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>) : (<Link className='option' to='/signin'> SIGN IN</Link>)
                 }
+                <CartIcon />
             </div>
+            {hidden ? null : <CartDropdown />}
         </div >
     )
 };
 
 //map state to props sends the state form our root-reducer, ( here user) and gives the props in Header
 //the new state from currentUser.  check the root reducer to see why state.user.currentUser is set.
-const mapStateToProps = state => ({
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
     //current user property that has a state of user.currentUser value;
-    currentUser: state.user.currentUser
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
